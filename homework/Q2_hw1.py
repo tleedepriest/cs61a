75ba77f485506ccccccf5692aa08b5cfd5c78d4b
tleedepriest <tleedepriest@gmail.com>
2019-10-06 12:12:35 -0400

    Update Q2_hw1.py
    made some spacing changes and removed the test cases

--- homework/Q2_hw1.py
+++ homework/Q2_hw1.py
@@ -1,7 +1,6 @@
 def two_of_three(a, b, c):
     """Return x*x + y*y, where x and y are the two largest members of the
     positive numbers a, b, and c.
-
     >>> two_of_three(1, 2, 3)
     13
     >>> two_of_three(5, 3, 1)
@@ -11,9 +10,4 @@ def two_of_three(a, b, c):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return max(a,b,c)*max(a,b,c) + max(min(b,c),min(a,b), min(c,a))**2
-
-two_of_three(1, 2, 3)
-two_of_three(5,3,1)
-two_of_three(10, 2, 8)
-two_of_three(5, 5, 5)
+    return max(a, b, c)*max(a, b, c) + max(min(b, c), min(a, b), min(c, a))**2
